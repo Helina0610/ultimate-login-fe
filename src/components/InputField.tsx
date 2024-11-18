2bf79b2a95a902741adb1957f827c4b16c9aef29
Helina0610 <hongjuy0610@gmail.com>
2024-11-18 15:14:48 +0900

    feat : login-1

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -6,14 +6,17 @@ interface InputFieidProps {
 	label  :string;
 	type  :string;
 	placeholder :string;
+	name : string;
+	value : string;
+	onChange : (event : React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const InputField = ({label, type, placeholder } : InputFieidProps) => {
+const InputField = ({label, type, placeholder, onChange ,name , value } : InputFieidProps) => {
 	return (
 		<div className='text-start'>
 			<label className='text-base font-semibold '>{label}</label>
 			<div className='flex mt-2 mb-6 h-12 w-full rounded-lg outline outline-2 outline-[#bcd8c3]'>
-					<input type={type} className='ml-4 w-[390px] outline-none' placeholder={placeholder}/>
+					<input type={type} name={name} value={value} className='ml-4 w-[390px] outline-none' placeholder={placeholder} onChange={event => onChange(event)}/>
 					{type === 'password' && 
 						<ShowYnButton/>
 					}
--- src/containers/LoginContainer.tsx
+++ src/containers/LoginContainer.tsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import InputField from '../components/InputField'
 import ButtonFieId from '../components/ButtonFieId'
 
+interface formDataProps {
+	email : string;
+	password : string;
+}
+
 const LoginContainer = () => {
+
+	const [formData, setFormData] = useState<formDataProps>({
+		email : '',
+		password : ''
+	});
+
+	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+	const onSubmit = (event : React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+	}
 	return (
 		<section className=' h-80 w-[422px] mt-7'>
-		<form>
-			<InputField label="Email" type="email" placeholder="Email" />
-			<InputField label="Password" type="password" placeholder="Password" />
+		<form onSubmit={onSubmit}>
+			<InputField  type="email" name='email' value={formData.email} label="Email" placeholder="Email" onChange={onChange}/>
+			<InputField  type="password" name='password' value={formData.password} label="Password"placeholder="Password" onChange={onChange} />
 			<div className='flex mt-6 justify-between'>
 				<div>
 					<input type='checkbox' className='accent-[#029664] size-4 inline-block align-middle'/>
