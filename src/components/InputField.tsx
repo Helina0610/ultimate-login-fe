59c36af946762cdc42db9d332aaf58be532cbdc1
Helina0610 <hongjuy0610@gmail.com>
2024-11-18 10:03:39 +0900

    feat : showYn button

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import ShowYnButton from './ShowYnButton';
+
 
 interface InputFieidProps {
 	label  :string;
@@ -10,9 +12,12 @@ const InputField = ({label, type, placeholder } : InputFieidProps) => {
 	return (
 		<div className='text-start'>
 			<label className='text-base font-semibold '>{label}</label>
-			<div className='mt-2 mb-6 h-12 w-full rounded-lg outline outline-2 outline-[#bcd8c3]'>
-					<input type={type} className='ml-4 mt-3 w-[390px] outline-none' placeholder={placeholder}/>
-				</div>
+			<div className='flex mt-2 mb-6 h-12 w-full rounded-lg outline outline-2 outline-[#bcd8c3]'>
+					<input type={type} className='ml-4 w-[390px] outline-none' placeholder={placeholder}/>
+					{type === 'password' && 
+						<ShowYnButton/>
+					}
+			</div>
 		</div>
 	)
 }
---
+++ src/components/ShowYnButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import showYn from '../assets/showYn.png'
+
+const ShowYnButton = () => {
+	return (
+		<a href=''>
+			<img src={showYn} className='w-6 h-6 mt-3.5 mr-3'></img>
+		</a>
+	)
+}
+
+export default ShowYnButton
